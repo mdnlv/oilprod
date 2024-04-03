89586109e1b94d20d57e6390e07d03b3655dce55
mdnlv <mikdnlv@gmail.com>
2024-04-03 17:50:33 +0500

    chore

---
+++ parsingXLSX/assets/script.js
@@ -0,0 +1,219 @@
+'use strict'
+
+// — он берет столбец, например «Ввод новых скважин», определяет какие он колонки занимает (подколонки) минут последную, 
+// тк обычно в ней находиться вычесляемые значения. Нужно убедиться что у всех колонок такой вид
+
+const parsingXLSX = {
+  // colIndex — 3
+  // colName — D
+  // rowIndex — 3
+  // rowName — 4
+  // address — { r:1, c:0 }
+  // a1Address — A2
+  // range — { s: { c: 0, r: 0 }, e: { c: 3, r: 2 } }
+  // a1Range — A1:D3
+
+  XLSX: null,
+  worksheet: null,
+
+  init(XLSX, dataFile, pageNumber) {
+    this.XLSX = XLSX
+    const workbook = this.XLSX.read(dataFile, { type: 'binary' })
+    const worksheetName = workbook.SheetNames[pageNumber - 1]
+    this.worksheet = workbook.Sheets[worksheetName]
+  },
+
+  parse(XLSX, dataFile, pageNumber, nameColList) {
+    // TODO:
+    // добавить проверку на кол-во страниц
+    // на существования страницы
+    // проdерить формат xls
+
+    this.init(XLSX, dataFile, pageNumber)
+
+    const indexSubColsOfCols = this.getIndexSubColsOfCols(nameColList)
+    const subRowDayObj = this.getSubRowDayObj()
+
+    return this.getResult(indexSubColsOfCols, subRowDayObj)
+  },
+
+  findAdressCellByName(name) {
+    for (const cellAddress in this.worksheet) {
+      if (!this.worksheet.hasOwnProperty(cellAddress)) continue
+
+      const cell = this.worksheet[cellAddress]
+      if (cell.v === name) {
+        return this.XLSX.utils.decode_cell(cellAddress)
+      }
+    }
+    return null
+  },
+
+  getMergedRange(cell) {
+    const merges = this.worksheet['!merges']
+
+    if (!merges || merges.lenght === 0) {
+      return null
+    }
+
+    for (const range of merges) {
+      if (
+        cell.r >= range.s.r &&
+        cell.r <= range.e.r &&
+        cell.c >= range.s.c &&
+        cell.c <= range.e.c
+      ) {
+        return range
+      }
+    }
+
+    return null
+  },
+
+  getColumsCell(valueCell) {
+    const adress = this.findAdressCellByName(valueCell)
+
+    if (!adress) {
+      return null
+    }
+
+    const mergedRange = this.getMergedRange(adress)
+
+    if (mergedRange) {
+      // TODO: fix old subCol
+      // const colums = new Array(mergedRange.e.c - mergedRange.s.c + 1) //
+      const colums = new Array(mergedRange.e.c - mergedRange.s.c)
+        .fill(null)
+        .map((_, i) => mergedRange.s.c + i)
+      return colums
+    } else {
+      return [adress.c]
+    }
+  },
+
+  getIndexSubColsOfCols(nameColList) {
+    return nameColList.reduce((acc, it) => {
+      acc[it] = this.getColumsCell(it)
+      return acc
+    }, {})
+  },
+
+  getSubRowDayObj() {
+    const subRowDayObj = {}
+    const daysAdress = this.getDaysAdress()
+
+    for (const day in daysAdress) {
+      const adressCell = daysAdress[day]
+      const mergedRange = this.getMergedRange(adressCell)
+
+      if (mergedRange) {
+        subRowDayObj[day] = this.getObjSubRowMergedDay(mergedRange)
+      } else {
+        subRowDayObj[day] = this.getObjSubRowDay(adressCell)
+      }
+    }
+
+    return subRowDayObj
+  },
+
+  getDaysAdress() {
+    const daysAdress = {}
+
+    const days = new Array(31)
+      .fill(null)
+      .map((_, i) => (i < 9 ? `0${i + 1}` : `${i + 1}`))
+
+    for (const a1Adress in this.worksheet) {
+      if (!this.worksheet.hasOwnProperty(a1Adress)) continue
+      if (a1Adress[0] !== 'A' || isNaN(Number(a1Adress[1]))) continue
+
+      const cell = this.worksheet[a1Adress]
+
+      if (!daysAdress.hasOwnProperty(cell.w) && days.includes(cell.w)) {
+        daysAdress[cell.w] = this.XLSX.utils.decode_cell(a1Adress)
+      }
+    }
+
+    return daysAdress
+  },
+
+  getObjSubRowMergedDay(mergedRange) {
+    const rowsIndex = new Array(mergedRange.e.r - mergedRange.s.r + 1)
+      .fill(null)
+      .map((_, i) => mergedRange.s.r + i)
+
+    return rowsIndex.reduce((acc, it) => {
+      const cell = this.getCell({
+        r: it,
+        c: mergedRange.s.c + 1
+      })
+      acc[cell.v] = it
+
+      return acc
+    }, {})
+  },
+
+  // TODO: протестировать
+  getObjSubRowDay(adressCell) {
+    const cell = this.getCell({
+      r: adressCell.r,
+      c: adressCell.c + 1
+    })
+
+    return { [cell.v]: adressCell.r }
+  },
+
+  getResult(indexSubColsOfCols, subRowDayObj) {
+    const result = {}
+
+    for (const colTitle in indexSubColsOfCols) {
+      const indexSubCols = indexSubColsOfCols[colTitle]
+
+      result[colTitle] = this.getDataCol(indexSubCols, subRowDayObj)
+    }
+
+    return result
+  },
+
+  getDataCol(indexSubCols, subRowDayObj) {
+    const dataCol = {}
+
+    for (const dayNumber in subRowDayObj) {
+      const propsWithIndexRows = subRowDayObj[dayNumber]
+
+      const dataSubCols = indexSubCols.reduce((acc, colIndex) => {
+        const dataWell = this.getDataWell(propsWithIndexRows, colIndex)
+        if (dataWell) acc.push(dataWell)
+
+        return acc
+      }, [])
+
+      if (dataSubCols.length > 0) {
+        dataCol[dayNumber] = dataSubCols
+      }
+    }
+
+    return dataCol
+  },
+
+  getDataWell(propsWithIndexRows, colIndex) {
+    const dataWell = {}
+
+    for (const props in propsWithIndexRows) {
+      const cell = this.getCell({
+        r: propsWithIndexRows[props],
+        c: colIndex
+      })
+
+      if (cell) dataWell[props] = cell.w
+    }
+
+    return Object.keys(dataWell).length !== 0 ? dataWell : null
+  },
+
+  getCell(adress) {
+    const a1Address = this.XLSX.utils.encode_cell(adress)
+
+    return this.worksheet[a1Address]
+  }
+}
